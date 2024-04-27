e8e46d7eb5d4b81dda7b35a4e1b6e23808a109a8
Lino Bustamante <masterlino@gmail.com>
2024-04-27 14:08:26 +0200

    hello world

---
+++ tRick sUI/tRick sUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tRick sUI
+//
+//  Created by Lino Bustamante on 27/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tRick sUI/tRick sUI/tRick_sUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  tRick_sUIApp.swift
+//  tRick sUI
+//
+//  Created by Lino Bustamante on 27/4/24.
+//
+
+import SwiftUI
+
+@main
+struct tRick_sUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
